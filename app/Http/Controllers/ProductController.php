0f4df9cfcc8efa65335070a2c0317444a51234d7
Borja <borjajaudenes@gmail.com>
2023-02-03 14:04:31 +0000

    Crear Productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,21 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index()
+    {
+        $products = Product::all()->toArray();
+        return array_reverse($products);
+    }
+
     public function store(Request $request)
     {
-        $product = new Product;
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->price = $request->price;
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'price' => 'required|numeric|max:1000',
+            'photo' => 'required|image|mimes:jpg,jpeg,png,gif,tif,svg',
+        ]);
 
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'photo' => $request->file('photo')->store(''),
+        ]);
 
-        if ($request->hasFile('photo')) {
-            $product->photo = $request->file('photo')->store('../resources/assets');
-        }
-        $product->save();
+        return response($product, Response::HTTP_CREATED);
+    }
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+    public function update($id, Request $request)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json('Product updated!');
+    }
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json('Product deleted!');
     }
 }
